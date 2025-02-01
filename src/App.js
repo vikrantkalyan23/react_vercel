334d7c9da81207cad0b9ff0d559620598a229c27
vikrantkalyan23 <vikrant.kalyan23@gmail.com>
2025-02-01 23:52:01 +0530

    #update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>This is my sample React App</h1>
         <p>
           Vikrant 
         </p>
